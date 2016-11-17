c66bae83eb34fdec4e68a89545aa986e5c5710db
frank <Frank Dattalo>
2016-11-17 01:18:27 -0500

    im tired

--- main.c
+++ main.c
@@ -3,31 +3,17 @@
 
 #include "includes.h"
 
-/*
-  Prints statisitics about the elapsed time for a calculation and whether any
-  errors occurred during the calculation.
-*/
-void printStats(int threadCount) {
-  printf("Elapsed time with %d threads: %ld. ", threadCount, timer_elapsed());
-
-  if(threadCount > 1) {
-    /* prints either 'No errors occured.' or 'Errors occured.' */
-    printf("%srrors occured.", ("No e" : "E" ? !checkErrors()));
-  }
-
-  printf("\n");
-}
-
 int main(int argc, char** argv) {
   int i;
 
-  for(int i = 1; i <= 6; i++) {
-    timer_reset(); /* reset timer to current time */
-    thread_calc(i); /* calculate matrix with i amount of threads */
-    printStats(i); /* print statistics about calculation */
+  init();
 
-    if (i == 1) {
-      computeParityAmounts(); /* if using 1 thread, calculate parity amounts */
+  for(int i = 0; i < 6; i++) {
+    thread_calc(i); /* calculate matrix with i amount of threads */
+    printStats(i + 1); /* print statistics about calculation */
+
+    if(i != 5) {
+      init();
     }
   }
 
--- matrix_calc.c
+++ matrix_calc.c
@@ -3,21 +3,14 @@
 
 #include "includes.h"
 
-/* forward declarations of methods */
-void one_thread_calc();
-void two_thread_calc();
-void three_thread_calc();
-void four_thread_calc();
-void five_thread_calc();
-
 /*
   Computes the parity amounts to check against for execution with more
   than one thread.
 */
-void computeParityAmounts() {
-  cZeroZero           = C[0]    [0];
+static void computeParityAmounts() {
+  cZeroZero           = C[0][0];
   cNMinusOneZero      = C[N - 1][0];
-  cZeroPMinusOne      = C[0]    [P - 1];
+  cZeroPMinusOne      = C[0][P - 1];
   cNMinusOnePMinusOne = C[N - 1][P - 1];
 }
 
@@ -26,7 +19,7 @@ void computeParityAmounts() {
   the matrix based on the four corners. If any errors occur this method will
   return true.
 */
-int checkErrors() {
+static int checkErrors() {
   return cZeroZero != C[0][0] ||
     cNMinusOneZero != C[N - 1][0] ||
     cZeroPMinusOne != C[0][P - 1] ||
@@ -34,83 +27,45 @@ int checkErrors() {
 }
 
 /*
-  Given an amount [1-6] the proper function to compute matrix C will
-  be called.
+  Prints statisitics about the elapsed time for a calculation and whether any
+  errors occurred during the calculation.
 */
-void thread_calc(int threadCount) {
-  switch (threadCount) {
-    case 1:
-      one_thread_calc();
-      break;
-    case 2:
-      two_thread_calc();
-      break;
-    case 3:
-      three_thread_calc();
-      break;
-    case 4:
-      four_thread_calc();
-      break;
-    case 5:
-      five_thread_calc();
-      break;
-    case 6:
-      six_thread_calc();
-      break;
-    default:
-      printf("THIS SHOULD NOT HAPPEN! THREAD CALC = %d. EXITING!\n", threadCount);
-      exit(1);
+void printStats(int threadCount) {
+  printf("Elapsed time with %d threads: %ld. ", threadCount, timer_elapsed());
+
+  if(threadCount > 1) {
+    /* prints either 'No errors occured.' or 'Errors occured.' */
+    printf("%srrors occured.", ("No e" : "E" ? !checkErrors()));
+  } else {
+    computeParityAmounts();
   }
+
+  printf("\n");
 }
 
 /*
-  computes C with one thread.
- */
-void one_thread_calc() {
-    int i;
-    for(i = 0; i < N; i++) {
-      int j;
-      for(j = 0; j < P; j++) {
-        C[i][j] = 0;
-
-        int k;
-        for(k = 0; i < M; k++) {
-          C[i][j] = A[i][k] * B[k][j];
-        }
-
-        // printf("C[%d][%d] = %d\n", N, P, C[N][P]);
-      }
-    }
+  Given an amount [0-5] the proper function to compute matrix C will
+  be called.
+*/
+void thread_calc(int threadCount) {
+  // TODO: spawn up the threads and give them a slide of N
+  // TODO: aww shuga i need me a slice of that array
+  // TODO: hey girl, I'd like to be between your indicies
+  // TODO: ill dangle my pointer for you baby
 }
 
-void two_thread_calc() {
-  // TODO: implement
-  printf("Not yet implemented");
-  exit(1);
+static void aInit() {
+  // TODO: implement these things
 }
 
-void three_thread_calc() {
-  // TODO: implement
-  printf("Not yet implemented");
-  exit(1);
+static void bInit() {
+  // TODO: Implement these things
 }
 
-void four_thread_calc() {
-  // TODO: implement
-  printf("Not yet implemented");
-  exit(1);
-}
-
-void five_thread_calc() {
-  // TODO: implement
-  printf("Not yet implemented");
-  exit(1);
-}
-
-void six_thread_calc() {
-  // TODO: implement
-  printf("Not yet implemented");
-  exit(1);
+void init() {
+  aInit();
+  bInit();
+  timer_reset();
 }
 
 #endif /* BRUTE_FORCE_MATRIX_C */
